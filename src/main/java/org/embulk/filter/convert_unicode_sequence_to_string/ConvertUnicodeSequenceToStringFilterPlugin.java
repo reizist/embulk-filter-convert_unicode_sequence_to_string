9ed78dc201104bf823397126fb32f9ba657ac39a
reizist <reizist@gmail.com>
2020-04-27 01:04:55 +0900

    ./gradlew checkstyle

--- src/main/java/org/embulk/filter/convert_unicode_sequence_to_string/ConvertUnicodeSequenceToStringFilterPlugin.java
+++ src/main/java/org/embulk/filter/convert_unicode_sequence_to_string/ConvertUnicodeSequenceToStringFilterPlugin.java
@@ -6,13 +6,13 @@ import org.embulk.config.ConfigException;
 import org.embulk.config.ConfigSource;
 import org.embulk.config.Task;
 import org.embulk.config.TaskSource;
-import org.embulk.spi.FilterPlugin;
-import org.embulk.spi.PageOutput;
-import org.embulk.spi.Schema;
-import org.embulk.spi.Page;
-import org.embulk.spi.PageReader;
-import org.embulk.spi.PageBuilder;
 import org.embulk.spi.Exec;
+import org.embulk.spi.FilterPlugin;
+import org.embulk.spi.Page;
+import org.embulk.spi.PageBuilder;
+import org.embulk.spi.PageOutput;
+import org.embulk.spi.PageReader;
+import org.embulk.spi.Schema;
 
 import java.util.List;
 
